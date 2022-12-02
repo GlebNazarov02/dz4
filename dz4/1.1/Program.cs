53f34e828539fb6481fe96615042bd07bacda4a3
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-02 17:25:32 +0300

    1st

---
+++ dz4/1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿void Step(double a, double b)
+{
+    double c = a;
+    if (b > 0)
+    {
+        for(int i = 1;i < b;i++)
+        {
+            a = a * c;
+        }
+        Console.WriteLine($"{a}");
+    }
+    else if (b < 0)
+    {
+        for(int i = 1;i <= (Math.Abs(b) + 1);i++)
+        {
+            a = a / c;
+        }
+        Console.WriteLine($"{a}");
+    }
+    else
+    {
+         Console.WriteLine("1");
+    }
+}
+Console.WriteLine("Число");
+double d = double.Parse(Console.ReadLine());
+Console.WriteLine("Степень");
+double f = double.Parse(Console.ReadLine());
+Step(d,f);
