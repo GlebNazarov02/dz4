f51ec81b1d55c07ce139b51a1b5d3824a2fe53c6
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-02 19:07:20 +0300

    2nd

---
+++ dz4/1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Kolvo(string b)
+{
+    int sum = 0;
+    int l = b.Length;
+    int chisl = int.Parse(b);
+    for (int i = 0; i < l; i++)
+    {
+        sum = sum + chisl%10;
+        chisl = chisl / 10;
+    }
+    Console.WriteLine($"{sum}");
+}
+Console.WriteLine("Число");
+string a = Console.ReadLine();
+Kolvo(a);
